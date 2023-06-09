b1b67d178cf26430d65bb2cd0b6299aaa8f0e651
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-09 19:48:25 -0400

    d2x: cherish-13 bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d2x.mk
+    $(LOCAL_DIR)/cherish_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d2x-eng \
-    lineage_d2x-user \
-    lineage_d2x-userdebug
+    cherish_d2x-eng \
+    cherish_d2x-user \
+    cherish_d2x-userdebug

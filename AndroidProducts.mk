759b31771cad68cd1adf30d33e0399ea6f2c175f
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-09 20:04:55 -0400

    d2x: bootleg bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The bootlegOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d2x.mk
+    $(LOCAL_DIR)/bootleg_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d2x-eng \
-    lineage_d2x-user \
-    lineage_d2x-userdebug
+    bootleg_d2x-eng \
+    bootleg_d2x-user \
+    bootleg_d2x-userdebug
